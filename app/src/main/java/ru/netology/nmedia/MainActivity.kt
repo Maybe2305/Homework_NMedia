89981376f33e9a1b78d442a18202846c26a9697d
Artem <xamidoff.job@gmail.com>
2024-03-31 17:49:29 +0300

    homework_5

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,73 +14,54 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            1,
-            "Нетология. Университет интернет-профессий будущего",
-            "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb/",
-            "21 Мая в 18:36",
-            false
-        )
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
 
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-
-            amountLikes.text = "999"
-            amountShares.text = "1095"
-            amountWatches.text = "1000"
-
-            if (post.likedByMe) {
-                ivLikes.setImageResource(R.drawable.baseline_favorite_24)
-            }
-
-            val intAmountLikes = amountLikes.text.toString().toInt()
-            var intAmountShares = amountShares.text.toString().toInt()
-            val intAmountWatches = amountWatches.text.toString().toInt()
-
-            amountLikes.text = textNumber(intAmountLikes)
-            amountShares.text = textNumber(intAmountShares)
-            amountWatches.text = textNumber(intAmountWatches)
-
-            ivLikes.setOnClickListener {
-                post.likedByMe = !post.likedByMe
+                amountLikes.text = textNumber(post.amountLikes)
+                amountShares.text = textNumber(post.amountShares)
+                amountWatches.text = textNumber(post.amountWatches)
 
                 ivLikes.setImageResource(
                     if (post.likedByMe) R.drawable.baseline_favorite_24
                     else R.drawable.baseline_favorite_border_24
                 )
 
-                if (post.likedByMe) amountLikes.text = textNumber(intAmountLikes + 1)
-                else amountLikes.text = textNumber(intAmountLikes - 1)
             }
-
-            ivShare.setOnClickListener {
-                amountShares.text = textNumber(++intAmountShares)
-            }
-
         }
 
+        binding.ivLikes.setOnClickListener {
+            viewModel.like()
+        }
 
-    }
-
-    fun textNumber(number: Int): String {
-        return when (number) {
-            in 1000..1099 -> "${number / 1000}K"
-            in 1100..9999 -> {
-                val thousands = number / 1000
-                val hundreds = (number % 1000) / 100
-                "$thousands.$hundreds K"
-            }
-
-            in 10000..999999 -> "${number / 1000}K"
-            in 1_000_000..Int.MAX_VALUE -> {
-                val thousands = number / 1000000
-                val hundreds = (number % 1000000) / 100000
-                "$thousands.$hundreds M"
-            }
-
-            else -> number.toString()
+        binding.ivShare.setOnClickListener {
+            viewModel.share()
         }
     }
+
 }
+
+fun textNumber(number: Int): String {
+    val convertedNumber = when (number) {
+        in 1000..1099 -> "${number / 1000}K"
+        in 1100..9999 -> {
+            val thousands = number / 1000
+            val hundreds = (number % 1000) / 100
+            "$thousands.$hundreds K"
+        }
+
+        in 10000..999999 -> "${number / 1000}K"
+        in 1_000_000..Int.MAX_VALUE -> {
+            val thousands = number / 1000000
+            val hundreds = (number % 1000000) / 100000
+            "$thousands.$hundreds M"
+        }
+
+        else -> number.toString()
+    }
+    return convertedNumber
+}
+
---
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -0,0 +1,14 @@
+package ru.netology.nmedia
+
+import android.widget.TextView
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likedByMe: Boolean,
+    var amountLikes: Int = 999,
+    val amountShares: Int = 999,
+    val amountWatches: Int = 150000,
+)
